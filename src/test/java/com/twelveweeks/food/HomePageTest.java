341e13916b362fbec4e5f119436fc856e7d60ebb
Ranunculus <tatianamalyutina@gmail.com>
2019-03-25 23:27:50 +0300

    WebMvcTest

---
+++ src/test/java/com/twelveweeks/food/HomePageTest.java
@@ -0,0 +1,30 @@
+package com.twelveweeks.food;
+
+import com.twelveweeks.food.controller.configuration.ViewConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+@ContextConfiguration(classes = ViewConfiguration.class)
+public class HomePageTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
+}
